610efaa2f4adcda19c8f2d79d91d949bceb3be11
Aluno Senac <Aluno@EDU72747LAB2PC.edu.sesc-ce.com.br>
2025-06-04 09:18:14 -0300

    resolvendo conflitos Cena: Menu, player, hud main 1

--- globais.gd
+++ globais.gd
@@ -10,10 +10,6 @@ var ultimaPorta: String = "Inicio"
 var ultima_pos_x = 572
 var ultima_pos_y = 373
 
-<<<<<<< HEAD
+
 var vida_maxima: int = 5
 var vida_atual: int = 5	
-=======
-var vida_maxima: int = 10
-var vida_atual: int = 10	
->>>>>>> 34a8c141a0b81a466691dd7f9fbfd595be42829d
